64ccf3a5f3205c9eec157a03fe8b7bc92479f074
jacobwshg <wsg438@gmail.com>
2026-04-07 21:18:16 -0500

    [L1] added more RWs

--- cs322-framework/L1/src/parser.h
+++ cs322-framework/L1/src/parser.h
@@ -3,6 +3,7 @@
 #define L1_PARSER_H
 
 #include <string_view>
+#include <array>
 
 namespace L1
 {
@@ -11,16 +12,30 @@ namespace L1
 	{
 		static constexpr std::string_view
 			LPAR { "(" },
-			RPAR { "(" },
+			RPAR { ")" },
 
-			LARR { "<-" },
-			MEM  { "mem" },
-			ADD_EQ { "+=" },
-			SUB_EQ { "-=" },
-			CJUMP { "cjump" },
-			GOTO { "goto" },
+			AT    { "@" },
+			COLON { ":" }
+
+			MEM { "mem" },
+
+			ADD { "+" },
+			SUB { "-" },
+			ASSIGN  { "<-" },
+			ADD_EQ  { "+=" },
+			SUB_EQ  { "-=" },
+			MUL_EQ  { "*=" },
+			BAND_EQ { "&=" },
+			LSH_EQ  { "<<=" },
+			RSH_EQ  { ">>=" },
+			LT      { "<" },
+			LEQ     { "<=" },
+			EQ      { "=" },
+
+			CJUMP  { "cjump" },
+			GOTO   { "goto" },
+			CALL   { "call" },
 			RETURN { "return" },
-			CALL { "call" },
 
 			PRINT { "print" },
 			INPUT { "input" },
@@ -30,25 +45,46 @@ namespace L1
 
 			INCR { "++" },
 			DECR { "--" },
-			AT { "@" },
 
 			RAX { "rax" },
 			RBX { "rbx" },
+			RCX { "rcx" },
+			RDX { "rdx" },
+			RDI { "rdi" },
+			RSI { "rsi" },
 			RBP { "rbp" },
+			RSP { "rsp" },
+
+			R8  { "r8" },
+			R9  { "r9" },
 			R10 { "r10" },
 			R11 { "r11" },
 			R12 { "r12" },
 			R13 { "r13" },
 			R14 { "r14" },
 			R15 { "r15" },
-			RDI { "rdi" },
-			RSI { "rsi" },
-			RDX { "rdx" },
-			R8  { "r8" },
-			R9  { "r9" }
 			;
-	}
 
+		static constexpr int LIBFUNC_CNT { 5 };
+		static constexpr std::array< std::string_view, LIBFUNC_CNT > LIBFUNCS 
+		{
+			PRINT, INPUT, ALLOCATE,
+			TUPLE_ERROR, TENSOR_ERROR,
+		};
+
+		static constexpr int W_REG_CNT { 9 };
+		static constexpr std::array< std::string_view, W_REG_CNT > W_REGS
+		{
+			RAX, RBX, RBP,
+			R10, R11, R12, R13, R14, R15,
+		};
+
+		static constexpr int A_REG_CNT { 5 };
+		static constexpr std::array< std::string_view, A_REG_CNT > A_REGS
+		{
+			RDI, RSI, RDX, R8, R9,
+		};
+	}
 }
 
 #endif
