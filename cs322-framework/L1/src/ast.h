41f4554aad3a74ccb59a355b653d60eb9133f036
jacobwshg <wsg438@gmail.com>
2026-04-09 16:23:49 -0500

    [L1] moved AST specification to separate file

---
+++ cs322-framework/L1/src/ast.h
@@ -0,0 +1,184 @@
+
+#ifndef L1_AST_H
+#define L1_AST_H
+
+#include <string_view>
+#include <array>
+#include <vector>
+#include <iostream>
+#include <variant>
+
+namespace L1
+{
+	/*
+	 * Reserved words
+	 */
+	namespace RW
+	{
+		static constexpr std::string_view
+			LPAR { "(" },
+			RPAR { ")" },
+			AT    { "@" },
+			COLON { ":" },
+
+			ADD { "+" },
+			SUB { "-" },
+			INCR { "++" },
+			DECR { "--" },
+			ASSIGN  { "<-" },
+			ADD_EQ  { "+=" },
+			SUB_EQ  { "-=" },
+			MUL_EQ  { "*=" },
+			BAND_EQ { "&=" },
+			LSH_EQ  { "<<=" },
+			RSH_EQ  { ">>=" },
+			LT      { "<" },
+			LEQ     { "<=" },
+			EQ      { "=" },
+
+			MEM { "mem" },
+
+			CJUMP  { "cjump" },
+			GOTO   { "goto" },
+			CALL   { "call" },
+			RETURN { "return" },
+
+			PRINT { "print" },
+			INPUT { "input" },
+			ALLOCATE { "allocate" },
+			TUPLE_ERROR { "tuple-error" },
+			TENSOR_ERROR { "tensor-error" },
+
+			RAX { "rax" },
+			RBX { "rbx" },
+			RCX { "rcx" },
+			RDX { "rdx" },
+			RDI { "rdi" },
+			RSI { "rsi" },
+			RBP { "rbp" },
+			RSP { "rsp" },
+			R8  { "r8" },
+			R9  { "r9" },
+			R10 { "r10" },
+			R11 { "r11" },
+			R12 { "r12" },
+			R13 { "r13" },
+			R14 { "r14" },
+			R15 { "r15" }
+			;
+
+		static constexpr int LIBFUNC_CNT { 5 };
+		static constexpr std::array< std::string_view, LIBFUNC_CNT >
+			LIBFUNCS 
+		{
+			RW::PRINT, RW::INPUT, RW::ALLOCATE,
+			RW::TUPLE_ERROR, RW::TENSOR_ERROR,
+		};
+
+		static constexpr int W_REG_CNT { 9 };
+		static constexpr std::array< std::string_view, W_REG_CNT >
+			W_REGS
+		{
+			RW::RAX, RW::RBX, RW::RBP,
+			RW::R10, RW::R11, RW::R12, RW::R13, RW::R14, RW::R15,
+		};
+
+		static constexpr int A_REG_CNT { 5 };
+		static constexpr std::array< std::string_view, A_REG_CNT >
+			A_REGS
+		{
+			RW::RDI, RW::RSI, RW::RDX, RW::R8, RW::R9,
+		};
+	}
+
+	template< typename ... Handlers >
+	struct NodeVisitor : Handlers...
+	{
+		using Handlers::operator()...;
+	};
+
+	struct LParNode {};
+	struct RParNode {};
+	struct AtNode {};
+	struct ColonNode {};
+
+	struct AddNode {};
+	struct SubNode {};
+	struct IncrNode {};
+	struct DecrNode {};
+	struct AssignNode {};
+	struct AddEqNode {};
+	struct SubEqNode {};
+	struct MulEqNode {};
+	struct BAndEqNode {};
+	struct LShEqNode {};
+	struct RShEqNode {};
+	struct LtNode {};
+	struct LEqNode {};
+	struct EqNode {};
+
+	struct MemNode {};
+	struct CJumpNode {};
+	struct GotoNode {};
+
+	struct CallNode {};
+	struct AllocateNode {};
+	struct TupleErrorNode {};
+	struct TensorErrorNode {};
+
+	struct RaxNode {};
+	struct RbxNode {};
+	struct RcxNode {};
+	struct RdxNode {};
+	struct RdiNode {};
+	struct RsiNode {};
+	struct RbpNode {};
+	struct RspNode {};	
+	struct R8Node  {};
+	struct R9Node  {};
+	struct R10Node {};
+	struct R11Node {};
+	struct R12Node {};
+	struct R13Node {};
+	struct R14Node {};
+	struct R15Node {};
+
+	struct _0Node {};
+	struct _1Node {};
+	struct _2Node {};
+	struct _3Node {};
+	struct _4Node {};
+	struct _8Node {};
+
+	struct NameNode { std::string_view name_re; };
+	struct LabelNode { ColonNode colon; NameNode name; };
+	struct LNode { AtNode at; NameNode name; };
+	struct NNode { std::string_view n_re; };
+	struct MNode { unsigned long M_val; };
+	struct FNode { unsigned long F_val; };
+	struct ENode { unsigned long E_val; };
+
+	using CmpNode = std::variant< LtNode, LEqNode, EqNode >;
+	using SOpNode = std::variant< LShEqNode, RShEqNode >;
+	using AOpNode = std::variant< AddEqNode, SubEqNode, MulEqNode, BAndEqNode >;
+
+	using SxNode = std::variant< RcxNode >;
+	using ANode = std::variant< RdiNode, RsiNode, SxNode, RdxNode, R8Node, R9Node >;
+	using WNode = std::variant< ANode, RaxNode, RbxNode, R10Node, R11Node, R12Node, R13Node, R14Node, R15Node >;
+
+	using XNode = std::variant< WNode, RspNode >;
+	using UNode = std::variant< WNode, LNode >;
+	using TNode = std::variant< XNode, NNode >;
+	using SNode = std::variant< TNode, LabelNode, LNode >;
+
+	struct IAssignNode { WNode w; AssignNode assign; SNode s; };
+	struct ILoadNode { WNode w; AssignNode assign; MemNode mem; XNode x; MNode M; };
+	struct IStoreNode { MemNode mem; XNode x; MNode M; AssignNode assign; SNode s; };
+	struct IAOpNode { WNode w; AOpNode aop; TNode t; };
+	struct ISOpSxNode { WNode w; SOpNode sop; SxNode sx; };
+	struct ISOpNNode { WNode w; SOpNode sop; NNode N; };
+
+}
+
+#endif
+
