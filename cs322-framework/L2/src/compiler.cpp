08f05fd65137249e55fb9491162b2033b91a9420
jacobwshg <wsg438@gmail.com>
2026-04-16 18:53:37 -0500

    [L2] fmt compiler.cpp

--- cs322-framework/L2/src/compiler.cpp
+++ cs322-framework/L2/src/compiler.cpp
@@ -1,157 +1,187 @@
-#include <algorithm>
-#include <assert.h>
-#include <cctype>
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
-#include <iterator>
-#include <set>
-#include <stdint.h>
-#include <string>
-#include <unistd.h>
-#include <utility>
-#include <vector>
 
 #include <utils.h>
 
-void print_help(char *progName) {
-  std::cerr << "Usage: " << progName
-            << " [-v] [-g 0|1] [-O 0|1|2] [-s] [-l] [-i] SOURCE" << std::endl;
-  return;
+#include <unistd.h>
+
+#include <algorithm>
+#include <assert.h>
+#include <cctype>
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <cstdint>
+#include <cstdlib>
+//#include <cstring>
+#include <string>
+#include <string_view>
+#include <utility>
+#include <vector>
+
+
+void print_help( const std::string_view progname )
+{
+	std::cerr
+		<< "Usage: "
+		<< progname << " [-v] [-g 0|1] [-O 0|1|2] [-s] [-l] [-i] SOURCE" << "\n";
+	return;
 }
 
-int main(int argc, char **argv) {
-  auto enable_code_generator = true;
-  auto spill_only = false;
-  auto interference_only = false;
-  auto liveness_only = false;
-  int32_t optLevel = 3;
+int main( int argc, char **argv )
+{
+	bool
+		enable_code_generator { true },
+		spill_only            { false },
+		interference_only     { false },
+		liveness_only         { false };
+	std::int32_t optLevel { 3 };
 
-  /*
-   * Check the compiler arguments.
-   */
-  Utils::verbose = false;
-  if (argc < 2) {
-    print_help(argv[0]);
-    return 1;
-  }
-  int32_t opt;
-  int64_t functionNumber = -1;
-  while ((opt = getopt(argc, argv, "vg:O:slif:")) != -1) {
-    switch (opt) {
+	/*
+	 * Check the compiler arguments.
+	 */
+	Utils::verbose = false;
+	if ( argc < 2 )
+	{
+		print_help( argv[0] );
+		return 1;
+	}
 
-    case 'l':
-      liveness_only = true;
-      break;
+	std::int32_t opt {};
+	std::int64_t functionNumber { -1 };
 
-    case 'i':
-      interference_only = true;
-      break;
+	while (
+		( opt = getopt( argc, argv, "vg:O:slif:" ) ) 
+		!= -1
+	)
+	{
+		switch ( opt )
+		{
 
-    case 's':
-      spill_only = true;
-      break;
+		case 'l':
+			liveness_only = true;
+			break;
 
-    case 'O':
-      optLevel = strtoul(optarg, NULL, 0);
-      break;
+		case 'i':
+			interference_only = true;
+			break;
 
-    case 'f':
-      functionNumber = strtoul(optarg, NULL, 0);
-      break;
+		case 's':
+			spill_only = true;
+			break;
 
-    case 'g':
-      enable_code_generator = (strtoul(optarg, NULL, 0) == 0) ? false : true;
-      break;
+		case 'O':
+			optLevel = std::strtoul( optarg, NULL, 0 );
+			break;
 
-    case 'v':
-      Utils::verbose = true;
-      break;
+		case 'f':
+			functionNumber = std::strtoul( optarg, NULL, 0 );
+			break;
 
-    default:
-      print_help(argv[0]);
-      return 1;
-    }
-  }
+		case 'g':
+			enable_code_generator = ( std::strtoul ( optarg, NULL, 0 ) == 0 ) 
+				? false : true;
+			break;
 
-  /*
-   * Parse the input file.
-   */
-  if (spill_only) {
+		case 'v':
+			Utils::verbose = true;
+			break;
 
-    /*
-     * Parse an L2 function and the spill arguments.
-     */
-    // TODO
+		default:
+			print_help( argv[ 0 ] );
+			return 1;
+		}
+	}
 
-  } else if (liveness_only) {
+	/*
+	 * Parse the input file.
+	 */
+	if ( spill_only )
+	{
 
-    /*
-     * Parse an L2 function.
-     */
-    // TODO
+		/*
+		 * Parse an L2 function and the spill arguments.
+		 */
+		// TODO
 
-  } else if (interference_only) {
+	}
+	else if ( liveness_only )
+	{
 
-    /*
-     * Parse an L2 function.
-     */
-    // TODO
+		/*
+		 * Parse an L2 function.
+		 */
+		// TODO
 
-  } else {
+	}
+	else if ( interference_only )
+	{
 
-    /*
-     * Parse the L2 program.
-     */
-    // TODO
-  }
+		/*
+		 * Parse an L2 function.
+		 */
+		// TODO
 
-  /*
-   * Special cases.
-   */
-  if (spill_only) {
+	}
+	else
+	{
 
-    /*
-     * Spill.
-     */
-    // TODO
+		/*
+		 * Parse the L2 program.
+		 */
+		// TODO
+	}
 
-    return 0;
-  }
+	/*
+	 * Special cases.
+	 */
+	if ( spill_only )
+	{
 
-  /*
-   * Liveness test.
-   */
-  if (liveness_only) {
-    // TODO
+		/*
+		 * Spill.
+		 */
+		// TODO
 
-    return 0;
-  }
+		return 0;
+	}
 
-  /*
-   * Interference graph test.
-   */
-  if (interference_only) {
-    // TODO
+	/*
+	 * Liveness test.
+	 */
+	if ( liveness_only )
+	{
+		// TODO
 
-    return 0;
-  }
+		return 0;
+	}
 
-  /*
-   * Print a single L2 function case.
-   */
-  if (functionNumber != -1) {
-    // TODO
+	/*
+	 * Interference graph test.
+	 */
+	if ( interference_only )
+	{
+		// TODO
 
-    return 0;
-  }
+		return 0;
+	}
 
-  /*
-   * Generate the target code normally.
-   */
-  if (enable_code_generator) {
-    // TODO
-  }
+	/*
+	 * Print a single L2 function case.
+	 */
+	if ( functionNumber != -1 )
+	{
+		// TODO
 
-  return 0;
+		return 0;
+	}
+
+	/*
+	 * Generate the target code normally.
+	 */
+	if ( enable_code_generator )
+	{
+		// TODO
+	}
+
+	return 0;
 }
+
