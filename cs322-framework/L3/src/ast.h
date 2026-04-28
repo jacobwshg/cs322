d48bb90be5dca064ebc3f74ce31cb0456fee449f
jacobwshg <wsg438@gmail.com>
2026-04-28 10:44:26 -0500

    [L3] added atomic nodes for AST

---
+++ cs322-framework/L3/src/ast.h
@@ -0,0 +1,471 @@
+
+#ifndef L2_AST_H
+#define L2_AST_H
+
+#include <string_view>
+#include <concepts>
+#include <array>
+#include <vector>
+#include <iostream>
+#include <variant>
+#include <regex>
+#include <tuple>
+#include <cstdint>
+
+namespace
+{
+
+	template< typename T >
+	using TypeTag = std::unique_ptr< T >;
+
+	/*
+	 * Two ways to check if a type is a std::variant< ... >
+	 *
+	 */
+	template< typename T >
+	struct is_variant: std::false_type
+	{};
+	// specialize `is_variant` for types that are `std::variant<...>`s of some other types
+	template< typename ... Ts >
+	struct is_variant< std::variant< Ts... > >: std::true_type
+	{};
+	template< typename T >
+	inline constexpr bool is_variant_v = is_variant< T >::value;
+
+	template< typename T >
+	concept IsVariant = requires( T t )
+	{
+		[]< typename ... Ts >
+		(
+			std::variant< Ts... >
+		) {}( t );
+	};
+
+	template< typename T >
+	struct is_tuple: std::false_type
+	{};
+	template< typename ... Ts >
+	struct is_tuple< std::tuple< Ts... > >: std::true_type
+	{};
+	template< typename T >
+	inline constexpr bool is_tuple_v = is_tuple< T >::value;
+
+	template< typename T >
+	struct is_vector: std::false_type
+	{};
+	template< typename T, typename A >
+	struct is_vector< std::vector< T, A > >: std::true_type
+	{};
+	template< typename T >
+	inline constexpr bool is_vector_v = is_vector< T >::value;
+}
+
+namespace L3
+{
+
+	using sv_t = std::string_view;
+
+	// whether a node represents a keyword
+	template< typename NodeT >
+	concept IsKWNode = requires
+	{
+		{ NodeT::kw } -> std::convertible_to< std::string_view >;
+	};
+
+	// whether a node represents a numeric constant
+	template< typename NodeT >
+	concept IsNumConstNode = requires
+	{
+		{ NodeT::val } -> std::convertible_to< long long >;
+	};
+
+	// whether a node represents an identifier token that matches a regular expression
+	// ( namely, a `name` or nonzero `N` token )
+	template< typename NodeT >
+	concept IsIdentNode = requires
+	{
+		{ NodeT::re } -> std::convertible_to< std::regex >;
+	};
+
+	// whether a node is a record (struct with subnodes as members)
+	template< typename NodeT >
+	concept IsRecNode = requires
+	{
+		true == ::is_tuple_v< typename NodeT::fields_t >;
+	};
+
+	static constexpr std::string_view EMPTYTOK { "" };
+
+	/*
+	 * Reserved words
+	 */
+	namespace KW
+	{
+		static constexpr std::string_view
+
+			DEFINE { "define" },
+
+			LPAR { "(" },
+			RPAR { ")" },
+			LCB  { "{" },
+			RCB  { "}" },
+
+			OP_ASSIGN  { "<-" },
+
+			LOAD  { "load" },
+			STORE { "store" },
+
+			RETURN { "return" },
+			BR     { "br" },
+			CALL   { "call" },
+	
+			PRINT    { "print" },
+			ALLOCATE { "allocate" },
+			INPUT    { "input" },
+			TUPLE_ERROR { "tuple-error" },
+			TENSOR_ERROR { "tensor-error" },
+
+			OP_ADD { "+" },
+			OP_SUB { "-" },
+			OP_MUL { "*" },
+			OP_AND { "&" },
+			OP_LSH { "<<" },
+			OP_RSH { ">>" },
+			OP_LT  { "<" },
+			OP_LE  { "<=" },
+			OP_EQ  { "=" },
+			OP_GE  { ">=" },
+			OP_GT  { ">" },
+
+			AT    { "@" },
+			COLON { ":" },
+			PERCENT { "%" },
+
+
+		static constexpr int LIBFUNC_CNT { 5 };
+		static constexpr std::array< std::string_view, LIBFUNC_CNT >
+			LIBFUNCS 
+		{
+			KW::PRINT, KW::INPUT, KW::ALLOCATE,
+			KW::TUPLE_ERROR, KW::TENSOR_ERROR,
+		};
+
+	}
+
+	struct defineNode { static constexpr sv_t kw { KW::DEFINE }; };
+
+	struct LParNode { static constexpr sv_t kw { KW::LPAR }; };
+	struct RParNode { static constexpr sv_t kw { KW::RPAR }; };
+	struct LCBNode  { static constexpr sv_t kw { KW::LCB }; };
+	struct RCBNode  { static constexpr sv_t kw { KW::RCB }; };
+
+	struct OpAssignNode { static constexpr sv_t kw { KW::OP_ASSIGN }; };
+
+	struct loadNode  { static constexpr sv_t kw { KW::LOAD  }; };
+	struct storeNode { static constexpr sv_t kw { KW::STORE }; };
+
+	struct returnNode { static constexpr sv_t kw { KW::RETURN }; };
+	struct brNode     { static constexpr sv_t kw { KW::BR }; };
+	struct callNode   { static constexpr sv_t kw { KW::CALL }; };
+
+	struct printNode       { static constexpr sv_t kw { KW::PRINT }; };
+	struct allocateNode    { static constexpr sv_t kw { KW::ALLOCATE }; };
+	struct inputNode       { static constexpr sv_t kw { KW::INPUT }; };
+	struct tupleerrorNode  { static constexpr sv_t kw { KW::TUPLE_ERROR }; };
+	struct tensorerrorNode { static constexpr sv_t kw { KW::TENSOR_ERROR }; };
+
+	struct OpAddNode { static constexpr sv_t kw { KW::OP_ADD }; };
+	struct OpSubNode { static constexpr sv_t kw { KW::OP_SUB }; };
+	struct OpMulNode { static constexpr sv_t kw { KW::OP_MUL }; };
+	struct OpAndNode { static constexpr sv_t kw { KW::OP_AND }; };
+	struct OpLshNode { static constexpr sv_t kw { KW::OP_LSH }; };
+	struct OpRshNode { static constexpr sv_t kw { KW::OP_RSH }; };
+	struct OpLtNode  { static constexpr sv_t kw { KW::OP_LT }; };
+	struct OpLeNode  { static constexpr sv_t kw { KW::OP_LE }; };
+	struct OpEqNode  { static constexpr sv_t kw { KW::OP_EQ }; };
+	struct OpGeNode  { static constexpr sv_t kw { KW::OP_GE }; };
+	struct OpGtNode  { static constexpr sv_t kw { KW::OP_GT }; };
+
+	struct AtNode    { static constexpr sv_t kw { KW::AT }; };
+	struct ColonNode { static constexpr sv_t kw { KW::COLON }; };
+	struct PercentNode { static constexpr sv_t kw { KW::PERCENT }; };
+
+	struct nameNode
+	{
+		static inline const std::regex re { "[a-zA-Z_][a-zA-z_0-9]*" };
+		//std::string_view val; // outlived by actual token in Parser::srcbuf
+		//
+		// switched to string because of temp nodes in spiller
+		//
+		std::string val;
+	};
+	struct varNode
+	{
+		using fields_t = std::tuple< PercentNode, nameNode >;
+		PercentNode percent_n; nameNode name_n;
+	};
+	struct labelNode
+	{
+		using fields_t = std::tuple< ColonNode, nameNode >;
+		ColonNode colon_n; nameNode name_n;
+	};
+	struct lNode
+	{
+		using fields_t = std::tuple< AtNode, nameNode >;
+		AtNode at_n; nameNode name_n;
+	};
+
+	struct NNode
+	{
+		static inline const std::regex re { "[+-]?[0-9][0-9]*" }; 
+		std::int64_t val;
+	};
+
+	struct cmpNode
+	{
+		enum class Id: int { LT=0, LE, EQ, GE, GT, };
+		static inline constexpr std::array< sv_t, 5 >
+			KWS { KW::LT, KW::LE, KW::EQ, KW::GE, KW::GT, };
+
+		Id id {};
+	};
+
+	struct opNode
+	{
+		enum class Id: int { ADD=0, SUB, MUL, AND, LSH, RSH, };
+		static inline constexpr std::array< sv_t, 6 >
+			KWS { KW::ADD, KW::SUB, KW::MUL, KW::AND, KW::LSH, KW::RSH };
+
+		Id id {};
+	};
+
+	using uNode = std::variant< varNode, lNode >;
+	using sNode = std::variant< tNode, labelNode, lNode >;
+
+	/*
+	// w <- s
+	struct iAssignNode 
+	{
+		using fields_t = std::tuple< wNode, OpAssignNode, sNode >;
+		wNode w_n; OpAssignNode op_assign_n; sNode s_n;
+	};
+	// w <- mem x M
+	struct iLoadNode
+	{
+		using fields_t = std::tuple< wNode, OpAssignNode, MemNode, xNode, MNode >;
+		wNode w_n;
+		OpAssignNode op_assign_n;
+		MemNode mem_n; xNode x_n; MNode M_n;
+	};
+	// mem x M <- s
+	struct iStoreNode
+	{
+		using fields_t = std::tuple< MemNode, xNode, MNode, OpAssignNode, sNode >;
+		MemNode mem_n; xNode x_n; MNode M_n;
+		OpAssignNode op_assign_n;
+		sNode s_n;
+	};
+	// w <- stack-arg M
+	struct iStackArgNode
+	{
+		using fields_t = std::tuple< wNode, OpAssignNode, StackArgNode, MNode >;
+		wNode w_n;
+		OpAssignNode op_assign_n;
+		StackArgNode stack_arg_n; MNode M_n;
+	};
+	// w aop t
+	struct iAOpNode
+	{
+		using fields_t = std::tuple< wNode, aopNode, tNode >;
+		wNode w_n; aopNode aop_n; tNode t_n;
+	};
+	// w sop sx
+	struct iSxNode
+	{
+		using fields_t = std::tuple< wNode, sopNode, sxNode >;
+		wNode w_n; sopNode sop_n; sxNode sx_n;
+	};
+	// w sop N
+	struct iSOpNode
+	{
+		using fields_t = std::tuple< wNode, sopNode, NNode >;
+		wNode w_n; sopNode sop_n; NNode N_n;
+	};
+	// mem x M += t
+	struct iAddStoreNode
+	{
+		using fields_t = std::tuple< MemNode, xNode, MNode, OpAddEqNode, tNode >;
+		MemNode mem_n;	xNode x_n; MNode M_n;
+		OpAddEqNode op_add_eq_n;
+		tNode t_n;
+	};
+	// mem x M -= t
+	struct iSubStoreNode
+	{
+		using fields_t = std::tuple< MemNode, xNode, MNode, OpSubEqNode, tNode >;
+		MemNode mem_n; xNode x_n; MNode M_n;
+		OpSubEqNode op_sub_eq_n;
+		tNode t_n;
+	};
+	// w += mem x M
+	struct iLoadAddNode	
+	{
+		using fields_t = std::tuple< wNode, OpAddEqNode, MemNode, xNode, MNode >;
+		wNode w_n;
+		OpAddEqNode op_add_eq_n;
+		MemNode mem_n; xNode x_n; MNode M_n;
+	};
+
+	// w -= mem x M
+	struct iLoadSubNode
+	{
+		using fields_t = std::tuple< wNode, OpSubEqNode, MemNode, xNode, MNode >;
+		wNode w_n;
+		OpSubEqNode op_sub_eq_n;
+		MemNode mem_n; xNode x_n; MNode M_n;
+	};
+	// w <- t cmp t
+	struct iCmpAssignNode
+	{
+		using fields_t = std::tuple< wNode, OpAssignNode, tNode, cmpNode, tNode >;
+		wNode w_n;
+		OpAssignNode op_assign_n;
+		tNode t1_n; cmpNode cmp_n; tNode t2_n;
+	};
+	// cjump t cmp t label
+	struct iCJumpNode
+	{
+		using fields_t = std::tuple< CJumpNode, tNode, cmpNode, tNode, labelNode >;
+		CJumpNode cjump_n;
+		tNode t1_n; cmpNode cmp_n; tNode t2_n;
+		labelNode label_n;
+	};
+	// label
+	struct iLabelNode
+	{
+		using fields_t = std::tuple< labelNode >;
+		labelNode label_n;
+	};
+	// goto label
+	struct iGotoNode
+	{
+		using fields_t = std::tuple< GotoNode, labelNode >;
+		GotoNode goto_n; labelNode label_n;
+	};
+	// return
+	struct iReturnNode
+	{
+		using fields_t = std::tuple< ReturnNode >;
+		ReturnNode return_n;
+	};
+	// call u N
+	struct iCallUNode
+	{
+		using fields_t = std::tuple< CallNode, uNode, NNode >;
+		CallNode call_n; uNode u_n; NNode N_n;
+	};
+	// call print 1
+	struct iCallPrintNode
+	{
+		using fields_t = std::tuple< CallNode, PrintNode, _1Node >;
+		CallNode call_n; PrintNode print_n; _1Node _1_n;
+	};
+	// call input 0
+	struct iCallInputNode
+	{
+		using fields_t = std::tuple< CallNode, InputNode, _0Node >;
+		CallNode call_n; InputNode input_n; _0Node _0_n;
+	};
+	// call allocate 2
+	struct iCallAllocateNode
+	{
+		using fields_t = std::tuple< CallNode, AllocateNode, _2Node >;
+		CallNode call_n; AllocateNode allocate_n; _2Node _2_n;
+	};
+	// call tuple-error 3
+	struct iCallTupleErrorNode
+	{
+		using fields_t = std::tuple< CallNode, TupleErrorNode, _3Node >;
+		CallNode call_n; TupleErrorNode tuple_error_n; _3Node _3_n;
+	};
+	// call tensor-error F
+	struct iCallTensorErrorNode
+	{
+		using fields_t = std::tuple< CallNode, TensorErrorNode, FNode >;
+		CallNode call_n; TensorErrorNode tensor_error_n; FNode F_n;
+	};
+	// w ++
+	struct iIncrNode
+	{
+		using fields_t = std::tuple< wNode, OpIncrNode >;
+		wNode w_n; OpIncrNode op_incr_n;
+	};
+	// w --
+	struct iDecrNode
+	{
+		using fields_t = std::tuple< wNode, OpDecrNode >;
+		wNode w_n; OpDecrNode op_decr_n;
+	};
+	// w @ w w E
+	struct iLEANode
+	{
+		using fields_t = std::tuple< wNode, AtNode, wNode, wNode, ENode >;
+		wNode w1_n;
+	 	AtNode at_n; 
+		wNode w2_n; wNode w3_n; ENode E_n;
+	};
+
+	using iNode = std::variant<
+
+		// `w <- s` could be a prefix of `w <- t cmp t`, and since make_variant_node() use short-circuit parsing,
+		// we must try matching the longer instruction first to avoid always parsing as the shorter one
+		//
+		iCmpAssignNode,
+		iAssignNode,
+
+		iLoadNode, iStoreNode,
+
+		iStackArgNode,
+
+		iAOpNode, iSxNode, iSOpNode,
+		iAddStoreNode, iSubStoreNode, iLoadAddNode, iLoadSubNode,
+
+		iCJumpNode, iLabelNode, iGotoNode,
+		iReturnNode,
+		iCallUNode, iCallPrintNode, iCallInputNode, iCallAllocateNode, iCallTupleErrorNode, iCallTensorErrorNode,
+		iIncrNode, iDecrNode, iLEANode
+	>;
+
+	struct builtin_argcnt
+	{
+		template< typename Node > static inline constexpr std::size_t val { 0 };
+		template<> inline constexpr std::size_t val< iCallPrintNode >      { 1 };
+		template<> inline constexpr std::size_t val< iCallAllocateNode >   { 2 };
+		template<> inline constexpr std::size_t val< iCallTupleErrorNode > { 3 };
+	};
+
+	struct fNode
+	{
+		using fields_t = std::tuple< LParNode, lNode, NNode, std::vector<iNode>, RParNode >;
+
+		LParNode lpar_n;
+		lNode l_n;
+		NNode N_n;
+		std::vector< iNode > i_ns;
+		RParNode rpar_n;
+	};
+
+	struct pNode
+	{
+		using fields_t = std::tuple< LParNode, lNode, std::vector< fNode >, RParNode >;
+
+		LParNode lpar_n;
+		lNode l_n;
+		std::vector< fNode > f_ns;
+		RParNode rpar_n;		
+	};
+	*/
+}
+
+#endif
+
